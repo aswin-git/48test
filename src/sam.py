2bcb8c0fb7990f9f783a8327155404c94c481094
Aswin S Kumar <aswinskumar26@gmail.com>
2026-04-15 17:13:01 +0530

    test comit

--- src/sam.py
+++ src/sam.py
@@ -1,5 +1,4 @@
 def add(num1 ,num2):
     ''' Add two numbers '''
-    
     num = num1 + num2
     return num
--- src/test_sum.py
+++ src/test_sum.py
@@ -2,5 +2,4 @@ from sam import add
 
 def test_sum():
     '''test the add'''
-    
     assert add(2,2) == 4
